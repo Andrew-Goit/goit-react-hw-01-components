12a7d21f711e652095eb0985c00b98bb1f2cc771
Andrew-Goit <105640738+Andrew-Goit@users.noreply.github.com>
2022-12-04 12:22:43 +0200

    first task

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+// import './App.css';
+
+import { Profile } from './Profile/Profile';
+import user from './initial/user.json';
+// import { Statistics } from './Statistics/Statistics';
+// import data from './initial/data.json';
+// import { FriendList } from './FriendList/FriendList';
+// import friends from './initial/friends.json';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+// import transactions from './initial/transactions.json';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} /> */}
+    </div>
+  );
+}
+
+export {App};
